2276e2239bfc4bbf6d4b59ff392329fa676f84c6
Alexander Karpov <alex.id.live@gmail.com>
2020-02-07 22:14:30 +0200

    Initial commit

---
+++ Example/Extensions/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Extensions
+//
+//  Created by Alexander Karpov on 02/07/2020.
+//  Copyright (c) 2020 Alexander Karpov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
